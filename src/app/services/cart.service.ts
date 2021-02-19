6ff96e6b0535520a18eb3dd2d3f0ec59719c5cec
zoltanmarai <zoltanmarai51@gmail.com>
2021-02-19 13:18:55 +0100

    addtocart works.

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -8,16 +8,19 @@
 })
 export class CartService {
     product: OrderedItem;
-  items = [];
+    items: OrderedItem[] | undefined;
 
   constructor(private http: HttpClient) {
+    // @ts-ignore
     this.product = {name: '', id: 0, price: 0, size: '', quantity: 0, subTotal: 0};
+    this.items = [];
   }
 
   // tslint:disable-next-line:typedef
   addToCart(product: OrderedItem) {
     // @ts-ignore
     this.items.push(product);
+    console.log(this.items);
   }
 
   // tslint:disable-next-line:typedef
